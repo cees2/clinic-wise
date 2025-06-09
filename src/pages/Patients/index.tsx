0fbde2e7fb882db1039b4050a3c487ce6cb4956b
Damian Mirek <damianmirek1234@gmail.com>
2025-06-09 16:33:21 +0200

    Add patients layout & update db types

--- src/pages/Patients/index.tsx
+++ src/pages/Patients/index.tsx
@@ -1,5 +1,43 @@
+import { Header } from "../../components/common/Header/Header";
+import TableDataRenderer from "../../components/layout/TableData/TableData";
+import { TableLayout } from "../../components/layout/TableData/TableLayout";
+import type { Tables } from "../../services/database.types";
+import type { TableDataConfig } from "../../utils/projectTypes";
+
 const Patients = () => {
-    return <div>Patients</div>;
+    const config: TableDataConfig<Tables<"patients">> = {
+        columns: [
+            {
+                id: "name",
+                name: "Name",
+                render: (patient) => `${patient.name} ${patient.surname}`,
+            },
+            {
+                id: "date_of_birth",
+                name: "Date of birth",
+            },
+            {
+                id: "gender",
+                name: "Gender",
+            },
+            {
+                id: "nationality",
+                name: "Nationality",
+            },
+            {
+                id: "phone_number",
+                name: "Phone number",
+            },
+        ],
+        resourceName: "patients",
+    };
+
+    return (
+        <TableLayout>
+            <Header as="h3" title="Patients" />
+            <TableDataRenderer config={config} />
+        </TableLayout>
+    );
 };
 
 export default Patients;
