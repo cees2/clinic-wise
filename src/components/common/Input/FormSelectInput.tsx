a4e54871828d38a5bc5b5fb4e3cdb18aef51ec84
Damian Mirek <damianmirek1234@gmail.com>
2025-06-22 18:26:11 +0200

    Visible selected option in a form select component

--- src/components/common/Input/FormSelectInput.tsx
+++ src/components/common/Input/FormSelectInput.tsx
@@ -3,6 +3,7 @@ import Select from "react-select";
 import type { OnChangeValue, Props as SelectProps } from "react-select";
 import AsyncSelect from "react-select/async";
 import { InputLabel } from "./common/InputCommon";
+import { useEffect, useRef } from "react";
 interface Props<OptionsType extends Record<string, any>, isMulti extends boolean, FormType extends Record<string, any>>
     extends SelectProps<OptionsType, isMulti> {
     options?: OptionsType[];
@@ -24,20 +25,48 @@ export const FormSelectInput = <
         field: { onChange, value, onBlur },
     } = useController({ name: registerName, control });
     const hasPredefinedOptions = Boolean(props.options);
+    const selectedValueFullObject = useRef<OptionsType | null >(null);
+
+    useEffect(() => {
+        if (!value) {
+            selectedValueFullObject.current = null;
+        }
+    }, [value]);
+
+    const onChangeInternal = (newValue: OnChangeValue<OptionsType, isMulti>) => {
+        selectedValueFullObject.current = newValue;
+        let newUpdatedValue;
+
+        if (props.getOptionValue) {
+            newUpdatedValue = props.getOptionValue(newValue);
+        } else if (newValue && "value" in newValue) {
+            newUpdatedValue = newValue.value;
+        } else {
+            newUpdatedValue = newValue;
+        }
+
+        onChange(newUpdatedValue);
+    };
 
     return (
         <div>
             <InputLabel htmlFor={registerName}>{label}</InputLabel>
             {hasPredefinedOptions ? (
-                <Select {...props} value={value} onChange={onChange} onBlur={onBlur} />
+                <Select
+                    {...props}
+                    value={selectedValueFullObject.current ?? value}
+                    onChange={onChangeInternal}
+                    onBlur={onBlur}
+                />
             ) : (
                 <AsyncSelect
                     {...props}
-                    value={value}
-                    onChange={onChange}
+                    value={selectedValueFullObject.current ?? value}
+                    onChange={onChangeInternal}
                     onBlur={onBlur}
                     defaultOptions
                     name={registerName}
+                    isClearable
                 />
             )}
         </div>
